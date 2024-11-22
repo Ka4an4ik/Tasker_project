e6c2afca36ba56383bdfac423b28083b2e087e49
Denis <den.mister.12@mail.ru>
2024-11-23 01:46:06 +0400

    links and views connected

--- dev_env/app1/tasker/urls.py
+++ dev_env/app1/tasker/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-
 from . import views
 
-app_name = 'tasker'
+app_name = 'tasker' 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),  
+    path("<int:task_id>/", views.task_view, name="view_task"),  
 ]
--- dev_env/app1/tasker/views.py
+++ dev_env/app1/tasker/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 
-# Create your views here.
 
 def index(request):
     task_list = Task.objects.all().order_by('deadline_time')
     current_time = timezone.now()
     return render(request, "tasker/index.html", {"task_list": task_list, "current_time": current_time})
+
+
+def task_view(request, task_id):
+    task = get_object_or_404(Task, task_id=task_id) 
+    return render(request, "tasker/event.html", {"task": task})  
