b258fe514bf06033f498c841d4d4ee6296fd375f
Denis <den.mister.12@mail.ru>
2025-01-21 23:32:47 +0400

    Auth with templates

--- dev_env/app1/app/urls.py
+++ dev_env/app1/app/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('index/', include("tasker.urls")),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
